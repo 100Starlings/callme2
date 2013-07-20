5d4d500a0ace5747545ead17f20fadc6787080cd
Riccardo Cambiassi <r.cambiassi@gmail.com>
2013-07-20 21:15:45 +0100

    customise activeadmin's Agent index
    * add action to steal "on call" status from other agents
    * display a list of devices for each agent

--- app/admin/agent.rb
+++ app/admin/agent.rb
@@ -6,19 +6,34 @@ ActiveAdmin.register Agent do
     Agent.find(selection).each do |agent|
       agent.on_call!
     end
+    redirect_to :back
   end
 
   batch_action :go_off_call do |selection|
     Agent.find(selection).each do |agent|
       agent.off_call!
     end
+    redirect_to :back
   end
 
   index do
+    selectable_column
     column :name, :sortable => :name do |agent|
       link_to agent.name, [:admin, agent]
     end
     column :on_call, :sortable => :on_call
+    column :devices do |agent|
+      ul do
+        agent.devices.each do |device|
+          li device.to_s
+        end
+      end
+    end
+    actions defaults: false do |agent|
+      if agent.off_call?
+        link_to "Go On Call", [:on_call_admin, agent], method: :put
+      end
+    end
   end
 
   show do |agent|
@@ -34,10 +49,18 @@ ActiveAdmin.register Agent do
     end
     active_admin_comments
   end
+
   controller do
     def permitted_params
       params.permit(agent: [:name, :on_call, { devices_attributes: [:name, :address, :id, :_destroy]}])
     end
+  end
+
+  member_action :on_call, :method => :put do
+    Agent.all.each(&:off_call!)
+    agent = Agent.find(params[:id])
+    agent.on_call!
+    redirect_to admin_agents_path, { notice: "Agent #{agent.name} is now on call!" }
   end
 
   form do |f|
