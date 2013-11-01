ccd7563195ada3fc51e83ce8442dcbb7ad3dea51
Riccardo Cambiassi <r.cambiassi@gmail.com>
2013-11-01 20:28:05 +0000

    get the secret tokens from the environment, please

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Callme2::Application.config.secret_key_base = '8fa91bb1bfaee69661c5128e471f7124c1014edc190bb8233fe0320b43e70342ebb684c3d7f01aaaa9920c9f3981f7ecf318b6c9d970126198343995fbe319a3'
+Callme2::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
