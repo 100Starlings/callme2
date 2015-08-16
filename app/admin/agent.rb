8b41065816848dacb570166f210889d060d143a1
Riccardo Cambiassi <riccardo@chaoscafe.co.uk>
2015-08-16 22:17:01 +0100

    simplify activeadmin views for agents

--- app/admin/agent.rb
+++ app/admin/agent.rb
@@ -1,63 +1,19 @@
 ActiveAdmin.register Agent do
-
   config.filters = false
-
-  batch_action :on_call do |selection|
-    Agent.find(selection).each(&:on_call!)
-    redirect_to :back
-  end
-
-  batch_action :off_call do |selection|
-    Agent.find(selection).each(&:off_call!)
-    redirect_to :back
-  end
 
   index do
     selectable_column
     column :name, sortable: :name do |agent|
       link_to agent.name, [:admin, agent]
     end
-    column :on_call, sortable: :on_call
-    column :devices do |agent|
-      ul do
-        agent.devices.each do |device|
-          li do
-            if device.active?
-              strong device.to_s
-            else
-              span class: "inactive" do
-                device.to_s
-              end
-            end
-          end
-        end
-      end
-    end
-    actions defaults: false do |agent|
-      if agent.off_call? && agent.ready?
-        link_to "Go On Call", [:on_call_admin, agent], method: :put
-      end
-    end
+    column :on_call_level, sortable: :on_call_level
   end
 
-  show do |agent|
+  show do |_agent|
     attributes_table do
       row :name
       row :email
-      row :on_call do
-        agent.on_call ? "YES" : "NO"
-      end
-      agent.devices.each do |device|
-        row device.name do
-          if device.active?
-            strong device.address
-          else
-            span class: "inactive" do
-              device.address
-            end
-          end
-        end
-      end
+      row :on_call_level
     end
     active_admin_comments
   end
@@ -65,29 +21,11 @@ ActiveAdmin.register Agent do
   controller do
     def permitted_params
       params.permit(agent:
-                    [:name,
-                     :email,
-                     :on_call,
-                     { devices_attributes:
-                       [:name, :address, :id, :active, :_destroy] },
-                    ])
-    end
-  end
-
-  member_action :on_call, method: :put do
-    agent = Agent.find(params[:id])
-    redirect_to(
-      admin_agents_path,
-      alert: "Agents need at least one active deice to be on call",
-    ) and return unless agent.ready?
-
-    Agent.all.each(&:off_call!)
-    if agent.on_call!
-      redirect_to admin_agents_path,
-        notice: "Agent #{agent.name} is now on call!"
-    else
-      redirect_to admin_agents_path,
-        alert: agent.errors.full_messages
+        [
+          :name,
+          :email,
+          :on_call_level,
+        ])
     end
   end
 
@@ -97,13 +35,7 @@ ActiveAdmin.register Agent do
       f.input :email
     end
     f.inputs "Status" do
-      f.input :on_call
-    end
-    f.has_many :devices do |device|
-      device.input :name
-      device.input :address
-      device.input :active
-      device.input :_destroy, as: :boolean, label: "Remove"
+      f.input :on_call_level
     end
 
     f.actions
