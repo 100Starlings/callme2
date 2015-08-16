f019f082c4d68a3dc72dbe9bf27fee1ed8691c96
Riccardo Cambiassi <riccardo@chaoscafe.co.uk>
2015-08-16 15:07:42 +0100

    dry up the twiml app a bit

--- lib/twiml_app.rb
+++ lib/twiml_app.rb
@@ -1,14 +1,14 @@
-require 'sinatra/base'
-require 'builder'
+require "sinatra/base"
+require "builder"
 
 class TwimlApp < Sinatra::Base
   enable :logging
 
-  use Rack::Auth::Basic, "Protected Area" do |username, password|
-    username == ENV['CALLME_USER'] && password == ENV['CALLME_PASS']
+  use Rack::Auth::Basic, "Callme Twilio App" do |username, password|
+    username == ENV["CALLME_USER"] && password == ENV["CALLME_PASS"]
   end
 
-  get '/' do
+  get "/" do
     logger.info "New call"
     if Agent.on_call.empty?
       redirect to("/voicemail")
@@ -17,28 +17,31 @@ class TwimlApp < Sinatra::Base
     end
   end
 
-  get '/call/:agents' do
-    logger.info "Calling #{params[:agents]} agents"
-    redirect to("/voicemail") unless ["active", "sleepers"].include?(params[:agents])
-    agents = agents_to_dial(params[:agents])
+  get "/call/:agents" do
+    type = params[:agents]
+    logger.info "Calling #{type} agents"
+    redirect to("/voicemail") unless %w(active sleepers).include?(type)
+    agents = agents_to_dial(type)
 
     builder do |xml|
       xml.response do
-        xml.say "Hello, please wait while we connect you to #{agents.map(&:name).join(' and ')}. ", voice: "woman"
-        xml.dial action: next_call(params[:agents]), method: "GET" do
+        names = agents.map(&:name).join(" and ")
+        xml.say "Hello, please wait while we connect you to #{names}",
+          voice: "woman"
+        xml.dial action: next_call(type), method: "GET" do
           numbers_to_dial(agents).each do |number|
-            xml.number number, url: "http://#{ENV['CALLME_USER']}:#{ENV['CALLME_PASS']}@#{request.host}/callme/screen"
+            xml.number number, url: callme_screen_url
           end
         end
       end
     end
   end
 
-  post '/screen' do
+  post "/screen" do
     logger.info "Screening call"
     builder do |xml|
       xml.response do
-        xml.gather action: "http://#{ENV['CALLME_USER']}:#{ENV['CALLME_PASS']}@#{request.host}/callme/complete_call" do
+        xml.gather action: callme_complete_url do
           xml.say "Press any key to accept this call", voice: "woman"
         end
         xml.hangup
@@ -46,7 +49,7 @@ class TwimlApp < Sinatra::Base
     end
   end
 
-  post '/complete_call' do
+  post "/complete_call" do
     logger.info "Completing call"
     builder do |xml|
       xml.response do
@@ -55,11 +58,12 @@ class TwimlApp < Sinatra::Base
     end
   end
 
-  get '/voicemail' do
+  get "/voicemail" do
     logger.info "Activating Voicemail"
-    message = "Welcome. I am sorry, our support team is not available at the moment.
-      Please leave a message including your phone number or email address after the beep.
-      Press star when finished"
+    message = "Welcome. I am sorry, our support team is not available
+      at the moment.
+      Please leave a message including your phone number or email address
+      after the beep. Press star when finished"
     builder do |xml|
       xml.response do
         xml.say message, voice: "woman"
@@ -68,19 +72,19 @@ class TwimlApp < Sinatra::Base
     end
   end
 
-  post '/recording' do
-    recording_url = params['RecordingUrl']
+  post "/recording" do
+    recording_url = params["RecordingUrl"]
     logger.info "Recording call at #{recording_url}"
     # email the recording url to the support team via sendhub.net ;)
     builder do |xml|
       xml.response do
-        xml.say "Thank you for contacting us.  We'll be in touch shortly.  Goodbye."
+        xml.say "Thank you for contacting us. We'll be in touch shortly. Goodbye"
         xml.hangup
       end
     end
   end
 
-  post '/callback' do
+  post "/callback" do
     # save stats for the call
     Call.create(status_callback_params)
   end
@@ -89,20 +93,32 @@ class TwimlApp < Sinatra::Base
 
   def next_call(agents)
     case agents
-    when 'active'
-      '/callme/call/sleepers'
-    when 'sleepers'
-      '/callme/voicemail'
+    when "active"
+      "/callme/call/sleepers"
+    when "sleepers"
+      "/callme/voicemail"
     end
   end
 
   def agents_to_dial(agents)
     case agents
-    when 'active'
+    when "active"
       Agent.on_call
-    when 'sleepers'
+    when "sleepers"
       Agent.off_call
     end
+  end
+
+  def callme_screen_url
+    "#{base_url}/callme/screen"
+  end
+
+  def callme_complete_url
+    "#{base_url}/callme/complete_call"
+  end
+
+  def base_url
+    "http://#{ENV["CALLME_USER"]}:#{ENV["CALLME_PASS"]}@#{request.host}"
   end
 
   def status_callback_params
@@ -114,11 +130,11 @@ class TwimlApp < Sinatra::Base
       direction:          params["Direction"],
       duration:           params["CallDuration"],
       recording_url:      params["RecordingUrl"],
-      recording_duration: params["RecordingDuration"]
+      recording_duration: params["RecordingDuration"],
     }
   end
 
   def numbers_to_dial(agents)
-    Device.active.where(agent_id: agents.map(&:id)).map(&:address)
+    Device.active.where(agent: agents).pluck(:address)
   end
 end
